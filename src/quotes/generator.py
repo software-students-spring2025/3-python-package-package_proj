bd25c5eb156374fa360847970c9cf183e20bd60a
andycabindol <andy.cabindol@gmail.com>
2025-03-13 14:40:56 -0400

    added src/

---
+++ src/quotes/generator.py
@@ -0,0 +1,13 @@
+QUOTES = []
+
+def get_random_quote():
+    return
+
+def get_random_quote_by_author(author):
+    return
+
+def add_quote(quote):
+    return
+
+def search_quotes(keyword):
+    return
